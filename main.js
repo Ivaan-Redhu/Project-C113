5307e528cf86315681fdf9b9fb5e6047c67270b5
Ivaan-Redhu <93902463+Ivaan-Redhu@users.noreply.github.com>
2023-03-18 15:40:36 +1300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+    save('frame.png');
+}
